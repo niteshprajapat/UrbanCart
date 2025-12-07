728be1882b73a74a98885991abab8cc5f24fb3ab
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-07 23:23:28 +0530

    Created Address model

---
+++ src/models/address.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    alternatePhone: {
+        type: String
+    },
+
+    pincode: {
+        type: String,
+        required: true,
+    },
+    houseNo: {
+        type: String,
+        required: true,
+    },
+    area: {
+        type: String,
+        required: true,
+    },
+    landmark: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    addressType: {
+        type: String,
+        enum: ["home", "work", "other"],
+        default: "home",
+    },
+
+}, {
+    timestamps: true,
+});
+
+
+const Address = mongoose.model("Address", addressSchema);
+export default Address;
