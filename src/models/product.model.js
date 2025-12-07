c3135ae7490c84e7786418d0cdd5a72a7def93d6
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-07 23:23:12 +0530

    Created Product model

---
+++ src/models/product.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    images: [
+        {
+            url: {
+                type: String,
+                default: "",
+            },
+            public_id: {
+                type: String,
+                default: "",
+            },
+        }
+    ],
+    isVerified: {
+        type: Boolean,
+        required: true,
+    },
+
+
+
+
+}, {
+    timestamps: true,
+});
+
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
