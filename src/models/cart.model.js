ad296e542b21838e01c9c95810be5561237d1d17
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-07 23:23:53 +0530

    Created Cart model

---
+++ src/models/cart.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    cartItems: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true,
+            },
+            name: {
+                type: String,
+            },
+            image: {
+                url: {
+                    type: String,
+                },
+                public_id: {
+                    type: String,
+                }
+            },
+            price: {
+                type: Numbere,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            }
+        }
+    ]
+
+
+}, {
+    timestamps: true,
+});
+
+
+const Cart = mongoose.model("Cart", cartSchema);
+export default Cart;
