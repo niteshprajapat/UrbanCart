63c7fa8c45848033da5ade54c46d11fa2e1626f3
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-10 23:09:38 +0530

    added Joi validation middleware

---
+++ src/middlewares/validate.js
@@ -0,0 +1,20 @@
+
+
+export const validate = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+        const errors = error.details.map((detail) => ({
+            field: detail.context.key,
+            message: detail.message,
+        }));
+
+        return res.status(400).json({
+            success: false,
+            message: "Validation failed",
+            errors,
+        });
+    }
+
+    next();
+}
