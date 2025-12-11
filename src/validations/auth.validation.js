3c425c9f065ffe98e7eec0993cf98fe9b4cc9217
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-11 23:33:50 +0530

    added login user validations

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -19,3 +19,17 @@ export const registerValidation = Joi.object({
     }),
     password: Joi.string(),
 })
+
+
+
+export const loginValidation = Joi.object({
+    usernameOrEmail: Joi.string().required().trim().lowercase().messages({
+        "string.empty": "Username or Email is required",
+        "any.required": "Username or Email is required",
+    }),
+    password: Joi.string().required().min(6).messages({
+        "string.empty": "Password is required",
+        "any.required": "Password is required",
+        "string.min": "Password must be at least 6 characters long",
+    })
+})
