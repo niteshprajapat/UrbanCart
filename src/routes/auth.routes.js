7d3b288052f17b2316aa431e54a6ef41fd765186
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-13 16:37:06 +0530

    created API route for verifyAccount

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 
 import { validate } from '../middlewares/validate.js';
-import { loginValidation, registerValidation } from '../validations/auth.validation.js';
+import { loginValidation, registerValidation, verifyAccountValidation } from '../validations/auth.validation.js';
 import { register, login, verifyAccount } from '../controllers/auth.controller.js';
 
 
 router.post("/register", validate(registerValidation), register);
 router.post("/login", validate(loginValidation), login);
-router.post("/verify-account", validate(loginValidation), verifyAccount);
+router.post("/verify-account", validate(verifyAccountValidation), verifyAccount);
 
 
 
