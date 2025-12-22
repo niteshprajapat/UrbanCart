9f207b3b1077e24aa8180b1a85412abc998b4189
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-22 23:24:22 +0530

    created API routes for forgotPassword, forgotPasswordWithLink, resetPassword, resetPassword2

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,13 +3,18 @@ const router = express.Router();
 
 
 import { validate } from '../middlewares/validate.js';
-import { loginValidation, registerValidation, verifyAccountValidation } from '../validations/auth.validation.js';
-import { register, login, verifyAccount } from '../controllers/auth.controller.js';
+import { loginValidation, registerValidation, resendVerificationTokenValidation, verifyAccountValidation } from '../validations/auth.validation.js';
+import { register, login, verifyAccount, resendVerificationToken, forgotPassword, resetPassword, forgotPasswordWithLink, resetPassword2 } from '../controllers/auth.controller.js';
 
 
 router.post("/register", validate(registerValidation), register);
 router.post("/login", validate(loginValidation), login);
 router.post("/verify-account", validate(verifyAccountValidation), verifyAccount);
+router.post("/resend-verification-token", validate(resendVerificationTokenValidation), resendVerificationToken);
+router.post("/forgot-password", forgotPassword);
+router.post("/forgot-password2", forgotPasswordWithLink);
+router.post("/reset-password", resetPassword);
+router.post("/reset-password2", resetPassword2);
 
 
 
