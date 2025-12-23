a239ba603272918da98820668aa7e72836dad899
Nitesh Prajapat <niteshp282000@gmail.com>
2025-12-23 23:24:10 +0530

    setup middlewares and api middlewares

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/database.js';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config({});
-
+connectDB();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+
+// middlewares
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+
+// routes middleware 
+app.use("/api/v1/auth", authRoutes);
+
 
 app.get("/", (req, res) => {
     return res.status(200).json({
@@ -17,3 +29,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
+
+
+
